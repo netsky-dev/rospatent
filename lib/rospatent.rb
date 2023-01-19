497c249b2f4d1e69afa7438e446d541822054514
netsky-dev <arenda244@yandex.ru>
2023-01-19 23:20:51 +0300

    init

---
+++ lib/rospatent.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rospatent/version"
+
+module Rospatent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rospatent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rospatent do
+  it "has a version number" do
+    expect(Rospatent::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
